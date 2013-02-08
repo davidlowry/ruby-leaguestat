a851c3898388f7c2e2ec4e76b56988a5df332f33
David Lowry <dave@twentyfour.local>
2013-02-08 23:25:30 +0000

    remove config stuff, it needs to be inline to work as a gem!

--- lib/config.rb
+++
@@ -1,9 +0,0 @@
-# require 'yaml'
-# 
-# puts "x #{File.join(File.dirname(__FILE__), '/../config/config.yml')}"
-# 
-# def read_config(file=File.expand_path(File.join(File.dirname(__FILE__), '../', '/config/config.yml')))
-#   puts File.expand_path(File.join(File.dirname(__FILE__), '../', '/config/config.yml'))
-#   config = YAML.load_file(file)
-#   return config['settings']
-# end
